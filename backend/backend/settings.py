ed06ed277d764c56d306110610ccb43b4bc5d19f
violetta-v <violettaverenova@yandex.ru>
2024-10-08 01:11:09 +0300

    fix flake8 errors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,16 +1,13 @@
-from pathlib import Path
 import os
-
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '172.25.55.17', '127.0.0.1']
-
 
 
 # Application definition
